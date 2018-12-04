9bcbfce59678a1c420bb5d36ea0d4e635ff7aa24
KukuruzaAndrey <andreyku6@gmail.com>
2018-12-04 19:29:37 +0000

    update day1/JS/second

--- day1/JavaScript/index2.js
+++ day1/JavaScript/index2.js
@@ -1,14 +1,27 @@
-`const { parseData, operationMap } = require('./utils')
+const { parseData, operationMap } = require('./utils')
 
-const frequencies = [operationMap[parseData[0].operation](0, parseData[0].number)]
-let i = 1
-while (true) {
-  const index = i % parseData.length
-  const frequency = operationMap[parseData[index].operation](frequencies[i - 1], parseData[index].number)
-  if (frequencies.includes(frequency)) {
-    console.log(frequency)
-    return
-  }
+const frequencies = []
+let i = 0
+let frequency = operationMap[parseData[0].operation](0, parseData[0].number)
+while (!frequencies.includes(frequency)) {
   frequencies[i] = frequency
   i += 1
+  const index = i % parseData.length
+  frequency = operationMap[parseData[index].operation](frequencies[i - 1], parseData[index].number)
 }
+console.log(frequency)
+
+// alternate //
+
+// const frequencies = [operationMap[parseData[0].operation](0, parseData[0].number)]
+// let i = 1
+// while (true) {
+//   const index = i % parseData.length
+//   const frequency = operationMap[parseData[index].operation](frequencies[i - 1], parseData[index].number)
+//   if (frequencies.includes(frequency)) {
+//     console.log(frequency)
+//     return
+//   }
+//   frequencies[i] = frequency
+//   i += 1
+// }
