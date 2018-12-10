f44eef1980ec41a13f37672455dd8674f528b3d9
KukuruzaAndrey <andreyku6@gmail.com>
2018-12-10 21:11:00 +0000

    day1-Py-fix path to input

--- day1/Python/main.py
+++ day1/Python/main.py
@@ -7,7 +7,7 @@ def formatter(varstr):
   return int(intstring) * mark
 
 def reader():
-  file1 = open("/day1/input","r")
+  file1 = open("../input","r")
   strarr = []
   strarr = file1.readlines()
   return strarr
